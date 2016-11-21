83c63d9cdb27012a9c5dc0db1c7e86f6d9a8376b
Bradenr <bradenr@iastate.edu>
2016-11-21 02:31:11 -0600

    Added some scanned photos.

---
+++ cpre-new/EX_register.vhd
@@ -0,0 +1,12 @@
+brary IEEE;
+use IEEE.std_logic_1164.all;
+
+entity EX_register is
+	port(	CLK		: in std_logic;
+			Reset	: in std_logic;
+			MemWr 	: in std_logic;
+			MemSign	: in std_logic;
+			MemHW	: in std_logic;
+			MemByte	: in std_logic;
+			MemWr	
+		);
