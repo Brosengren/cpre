6b3294bb09fa4be7e68577c70d7fd17cdd33a417
Veneciaaa <valvarez@iastate.edu>
2016-12-11 18:47:03 -0600

    load use and branches will have a diff signal

--- cpre-new/hazarddetection.vhd
+++ cpre-new/hazarddetection.vhd
@@ -4,9 +4,9 @@ use IEEE.std_logic_1164.all;
 entity hazarddetection is
 	port( IF_Rs        : in std_logic_vector(4 downto 0);
 				IF_Rt        : in std_logic_vector(4 downto 0);
-				ID_MemRead   : in std_logic; 
+				ID_MemRead   : in std_logic;
 				ID_Rt        : in std_logic_vector(4 downto 0);
-				Stall        : out std_logic);
+				LoadUse_Hazard : out std_logic);
 end hazarddetection;
 
 architecture beevee of hazarddetection is
@@ -18,10 +18,12 @@ architecture beevee of hazarddetection is
 	begin
 	if ( (ID_MemRead = '1')
 	and ((ID_Rt = IF_Rs) or (ID_Rt = IF_Rt)) ) then
-		Stall <= '1';
+		LoadUse_Hazard <= '1';
 	else
-		Stall <= '0';
+		LoadUse_Hazard <= '0';
 	end if;
 	end process;
+
+	--Branch
 
 end beevee;
